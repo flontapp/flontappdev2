d2a933dee36b2eedc265fa2e9b15f6e749a82cbb
flontapp <flont.app@yahoo.com>
2017-04-25 17:52:21 -0500

    Add files via upload

---
+++ picogram.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v12.2.1 (64 bit)
+MySQL - 5.5.47-0ubuntu0.14.04.1 : Database - vindnpico
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`vindnpico` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `vindnpico`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `aid` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Admin id',
+  `admin_username` varchar(30) NOT NULL COMMENT 'admin username',
+  `admin_password` varchar(30) NOT NULL COMMENT 'admin password',
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`aid`,`admin_username`,`admin_password`) values 
+(2,'admin','toptheglobal'),
+(3,'admin','admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
